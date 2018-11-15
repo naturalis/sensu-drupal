6f444efcd92cf9bf66c79fd37b8539e5bbeaa83b
Joep Vermaat <joep.vermaat@gmail.com>
2018-11-15 16:07:12 +0100

    Sudo fix

--- scripts/watchdogcheck.sh
+++ scripts/watchdogcheck.sh
@@ -6,22 +6,20 @@ COUNT_SEVERE=0
 COUNT_NORMAL=0
 SEVERE_ERRORS="0 1"
 NORMAL_ERRORS="2 3"
-cd /opt/docker-drupal
 if [ ! $FORMAT ] ; then
     FORMAT='list'
 fi
 for lvl in $NORMAL_ERRORS; do
     while read -r line; do
         COUNT_NORMAL=$((COUNT_NORMAL+1))
-    done < <(sudo docker-compose exec -T drupal drush ws --severity=$lvl --format=$FORMAT 2> /dev/null)
+    done < <(sudo sh -c "cd /opt/docker-drupal;docker-compose exec -T drupal drush ws --severity=$lvl --format=$FORMAT" 2> /dev/null)
 done
 for lvl in $SEVERE_ERRORS; do
     while read -r line; do
         COUNT_SEVERE=$((COUNT_SEVERE+1))
-    done < <(sudo docker-compose exec -T drupal drush ws --severity=$lvl --format=$FORMAT 2> /dev/null)
+    done < <(sudo sh -c "cd /opt/docker-drupal;docker-compose exec -T drupal drush ws --severity=$lvl --format=$FORMAT" 2> /dev/null)
 done
 echo "Log messages: $COUNT_SEVERE severe, $COUNT_NORMAL normal"
-cd -
 if [ $COUNT_SEVERE -gt 0 ]
 then
     exit 2

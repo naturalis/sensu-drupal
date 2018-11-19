b002bf809a5a1d05b4293402601264dfb4cbe38b
Joep Vermaat <joep.vermaat@gmail.com>
2018-11-19 11:00:21 +0100

    Improved update check (did not work with docker)

--- scripts/updatecheck.sh
+++ scripts/updatecheck.sh
@@ -5,14 +5,8 @@
 SECURITYUPDATES=0
 NORMALUPDATES=0
 cd /opt/docker-drupal
-while read -r line
-do
-    SECURITYUPDATES=$((SECURITYUPDATES+1))
-done < <(docker-compose exec -T drupal drush ups --format=list --security-only 2> /dev/null )
-while read -r line
-do
-    NORMALUPDATES=$((NORMALUPDATES+1))
-done < <(docker-compose exec -T drupal drush ups --format=list 2> /dev/null)
+SECURITYUPDATES=$(docker-compose exec drupal drush ups --security-only --pipe |wc -l)
+NORMALUPDATES=$(docker-compose exec -T drupal drush ups --pipe |wc -l )
 echo "Updates: $SECURITYUPDATES security, $NORMALUPDATES normal"
 if [ $SECURITYUPDATES -gt 0 ]
 then
